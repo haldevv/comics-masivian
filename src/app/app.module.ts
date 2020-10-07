381f384a87e2d6bc8ed3449cd09acac5775aacba
Jhon Harold Yepes <jhonyepes.e27@gmail.com>
2020-10-07 18:28:23 -0500

    Created app screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatCardModule} from '@angular/material/card'
+import {MatGridListModule} from '@angular/material/grid-list'
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon'
+
+import { RatingListComponent } from './components/rating-list/rating-list.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RatingListComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
